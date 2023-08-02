2031a5d4c33a9b7cdc0820f74750c7f566bfe267
Emmanuel Morales <emorales@interware.com.mx>
2023-08-01 20:11:32 -0500

    changing port

--- hello-angular/server/server.js
+++ hello-angular/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, '../dist/hello-angular')));
